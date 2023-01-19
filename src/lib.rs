41d85aca024ef16e77dde05000cde8450dc6d02e
bluespada <pentingmain@gmail.com>
2023-01-19 16:18:08 +0800

    update metadata

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use openagro_extension::extension::ExtensionMetadata;
+
+
+pub fn metadata() -> ExtensionMetadata {
+    ExtensionMetadata {
+        extension_id: "openagro.extension.sales".to_string(),
+        name: "Sales".to_string(),
+        description: None,
+        category: None,
+        author: "bluespada".to_string(),
+        version: "1.0.0".to_string(),
+        depends_on: vec![],
+        application: true,
+    }
+}
